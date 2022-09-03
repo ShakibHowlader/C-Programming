baac045458fe787e5a307d4b19602cebb718d2f7
ShakibHowlader <98587327+ShakibHowlader@users.noreply.github.com>
2022-09-03 08:16:11 -0700

    float type or double type user input
    getting float type or double type user input in c programming

---
+++ double_scanf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   double gpa;
+       printf("Enter your gpa:");
+       scanf("%lf",&gpa);/*"&" is an pointer*/
+       printf("Your gpa is %f\n",gpa);
+  
+    return 0;
+}
