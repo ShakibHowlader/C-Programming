00964e77f024da0f14b5f148c8331e276b4a280a
ShakibHowlader <98587327+ShakibHowlader@users.noreply.github.com>
2022-09-03 08:14:01 -0700

    getting int user input
    getting int user input in c

---
+++ int_scanf.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+int main()
+{
+   int age ;
+   printf("Enter your age : ");
+   scanf("%d",&age);/*"&" is an pointer*/
+   printf("You are %d years old\n",age);
+  
+    return 0;
+}
