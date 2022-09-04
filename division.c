301a5fe2eedad4a53c345dc69b3dc38495c3e712
ShakibHowlader <98587327+ShakibHowlader@users.noreply.github.com>
2022-09-04 08:08:35 -0700

    division in c
    taking user input and devise them

---
+++ division.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,div;
+
+    printf("Enter first number : ");
+    scanf("%d",&a);
+
+    printf("Enter second number : ");
+    scanf("%d",&b);
+
+    div = a / b;
+
+    printf("Answer : %d",div);
+
+    return 0;
+}
