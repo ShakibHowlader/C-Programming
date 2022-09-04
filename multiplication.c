d639b0d3165190ff8b84e2c45d7da24ba040a62a
ShakibHowlader <98587327+ShakibHowlader@users.noreply.github.com>
2022-09-04 08:05:00 -0700

    multiplication of two numbers
    Taking user input and multiply

---
+++ multiplication.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,mul;
+
+    printf("Enter first number : ");
+    scanf("%d",&a);
+
+    printf("Enter second number : ");
+    scanf("%d",&b);
+
+    mul = a * b;
+
+    printf("Multiplication : %d",mul);
+
+    return 0;
+}
